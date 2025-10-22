169a75746d106f4d362a150855b27714b9ae7805
Prashwarmishra <prashwar.mishra@jiostar.com>
2025-10-22 12:26:04 +0530

    change layout from a popup to a sidebar

---
+++ background/background.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener((tab) => {
+  chrome.sidePanel.open({ windowId: tab.windowId });
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
+
